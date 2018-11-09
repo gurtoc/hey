394a21d1ed9f1e851544114c76a7c91202a63889
gurtoc <gurtoc@gmail.com>
2018-11-09 16:11:02 +0100

    Uzupelnilem calc

--- hey/Program.cs
+++ hey/Program.cs
@@ -8,8 +8,45 @@ namespace hey
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-        }
+
+            Console.WriteLine("Witam w kalkulatorze");
+            Console.WriteLine("Wybierz liczbe a");
+            
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Wybierz liczbę b");
+            int b = int.Parse(Console.ReadLine());
+         
+
+  
+            String znak;
+            Console.WriteLine("Wybierz rodzaj działania arytmetycznego: +,-,*,/");
+            znak = Console.ReadLine();
+            Console.WriteLine(znak);
+
+            switch (znak)
+            {
+                case "+" :
+                    Console.WriteLine("twoj znak to: "+ znak);
+                    Console.WriteLine("Wynik dodawania a+b="+(a+b));
+                    break;
+                case "-":
+                    Console.WriteLine("twoj znak to:" + znak );
+                    Console.Write("Wynik odejmowania a-b=" + (a - b));
+                    break;
+                case "*":
+                    Console.WriteLine("twoj  znak to:"+znak );
+                    Console.WriteLine("Wynik mnożenia a*b="+a*b);
+                    break;
+                case "/":
+                    Console.WriteLine("twoj znak to:"+znak);
+                    Console.WriteLine("Wynik dzielenia a/b="+a/b);
+                    break;
+            }
+            Console.ReadLine();
+        }   
     }
 }
