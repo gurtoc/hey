105554b9ca8617bb458a5323dd6901f556ae1921
gurtoc <gurtoc@gmail.com>
2018-11-09 15:27:50 +0100

    Dodaj pliki projektów.

---
+++ hey/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
